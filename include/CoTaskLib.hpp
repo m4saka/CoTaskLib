c4ea38f7132237f3be1a7d8ed4c5f11c6f6a88b6
masaka <masaka@mz-kb.com>
2024-05-05 22:27:09 +0900

    内部でしか使わないconceptはdetail名前空間に入れる

--- include/CoTaskLib.hpp
+++ include/CoTaskLib.hpp
@@ -2117,11 +2117,14 @@ inline namespace cotasklib
 			return AsTask<detail::SimpleFadeOutSequence>(duration, color, drawOrder);
 		}
 
-		template <typename T>
-		concept IsBasicStringView = std::is_same_v<T, std::basic_string_view<typename T::value_type, typename T::traits_type>>;
+		namespace detail
+		{
+			template <typename T>
+			concept IsBasicStringView = std::is_same_v<T, std::basic_string_view<typename T::value_type, typename T::traits_type>>;
 
-		template <typename T>
-		concept IsSpan = std::is_same_v<T, std::span<typename T::value_type>>;
+			template <typename T>
+			concept IsSpan = std::is_same_v<T, std::span<typename T::value_type>>;
+		}
 
 		template <detail::SceneConcept TScene, typename... Args>
 		[[nodiscard]]
@@ -2132,15 +2135,15 @@ inline namespace cotasklib
 				"Scene constructor arguments must be copy-constructible.");
 
 			// std::basic_string_viewは許可しない
-			static_assert(!(IsBasicStringView<Args> || ...),
-				"std::basic_string_view is not allowed as a scene constructor argument because it is not deep-copied, risking dangling references. Use std::string instead.");
+			static_assert(!(detail::IsBasicStringView<Args> || ...),
+				"std::basic_string_view is not allowed as a scene constructor argument because it is not deep-copied, risking dangling references. Use std::basic_string instead.");
 
 			// StringViewは許可しない
 			static_assert(!(std::is_same_v<std::decay_t<Args>, StringView> || ...),
 				"StringView is not allowed as a scene constructor argument because it is not deep-copied, risking dangling references. Use String instead.");
 
 			// std::spanは許可しない
-			static_assert(!(IsSpan<Args> || ...),
+			static_assert(!(detail::IsSpan<Args> || ...),
 				"std::span is not allowed as a scene constructor argument because it is not deep-copied, risking dangling references. Use std::vector or similar.");
 
 			return [=] { return std::make_unique<TScene>(args...); };
